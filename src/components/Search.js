a421b3e3f3958d2096b28bfc1168bfa348314332
Benson <benson.odongo@student.moringaschool.com>
2022-10-07 15:38:16 +0300

    added search component

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = ({ searchMovies, handleChange }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Movies"
+        value={searchMovies}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Search;
